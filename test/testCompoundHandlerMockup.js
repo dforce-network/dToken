d855cc47ce850d8bde0ea065eae898473a2b0cd3
Hui Min HE <joyioushe@gmail.com>
2020-06-19 00:17:41 +0800

    New test files for CompoundHandlerMockup and dToken integration

---
+++ test/testCompoundHandlerMockup.js
@@ -0,0 +1,261 @@
+const CompoundHandler = artifacts.require("CompoundHandler");
+const CToken = artifacts.require("CTokenMockup");
+const FiatToken = artifacts.require("FiatTokenV1");
+const dTokenAddresses = artifacts.require("dTokenAddresses");
+const truffleAssert = require("truffle-assertions");
+const BN = require("bn.js");
+const UINT256_MAX = new BN(2).pow(new BN(256)).sub(new BN(1));
+
+describe("CompoundHandlerMockup contract", function () {
+  let owner, account1, account2, account3, account4;
+  let USDC;
+  let handler;
+  let dtoken_addresses;
+  let mock_dtoken = "0x0000000000000000000000000000000000000001";
+
+  before(async function () {
+    [
+      owner,
+      account1,
+      account2,
+      account3,
+      account4,
+    ] = await web3.eth.getAccounts();
+  });
+
+  async function resetContracts() {
+    dtoken_addresses = await dTokenAddresses.new();
+    handler = await CompoundHandler.new(dtoken_addresses.address);
+    USDC = await FiatToken.new(
+      "USDC",
+      "USDC",
+      "USD",
+      6,
+      owner,
+      owner,
+      owner,
+      owner,
+      {
+        from: owner,
+      }
+    );
+    await USDC.allocateTo(handler.address, 100000e6, {
+      from: owner,
+    });
+
+    let cUSDC = await CToken.new("cUSDC", "cUSDC", USDC.address);
+    handler.setcTokensRelation([USDC.address], [cUSDC.address]);
+
+    await handler.approve(USDC.address);
+
+    await dtoken_addresses.setdTokensRelation([USDC.address], [mock_dtoken]);
+    await handler.enableTokens([USDC.address]);
+  }
+
+  describe("Deployment", function () {
+    it("Should deployed and only initialized once", async function () {
+      await resetContracts();
+
+      await truffleAssert.reverts(
+        handler.initialize(dtoken_addresses.address, {
+          from: owner,
+        }),
+        "initialize: Already initialized!"
+      );
+    });
+  });
+
+  describe("setdTokens", function () {
+    it("Should only allow auth to set dTokens", async function () {
+      let new_dtoken_addresses = await dTokenAddresses.new();
+      await handler.setdTokens(new_dtoken_addresses.address);
+
+      await truffleAssert.reverts(
+        handler.setdTokens(new_dtoken_addresses.address, {
+          from: account1,
+        }),
+        "ds-auth-unauthorized"
+      );
+    });
+
+    it("Should not allow set dTokens with the same address", async function () {
+      await truffleAssert.reverts(
+        handler.setdTokens(await handler.dTokens()),
+        "setdTokens: The same dToken mapping contract address!"
+      );
+    });
+  });
+
+  describe("disableToken/enableToken", function () {
+    before(async function () {
+      await resetContracts();
+    });
+
+    it("Should only allow auth to disable token", async function () {
+      await handler.disableTokens([USDC.address]);
+      assert.equal(await handler.tokenIsEnabled(USDC.address), false);
+
+      await truffleAssert.reverts(
+        handler.disableTokens([USDC.address], {
+          from: account1,
+        }),
+        "ds-auth-unauthorized"
+      );
+    });
+
+    it("Should not allow to disable token already disabled", async function () {
+      await truffleAssert.reverts(
+        handler.disableTokens([USDC.address]),
+        "disableToken: Has been disabled!"
+      );
+    });
+
+    it("Should only allow auth to enable token", async function () {
+      await handler.enableTokens([USDC.address]);
+      assert.equal(await handler.tokenIsEnabled(USDC.address), true);
+
+      await truffleAssert.reverts(
+        handler.enableTokens([USDC.address], {
+          from: account1,
+        }),
+        "ds-auth-unauthorized"
+      );
+    });
+
+    it("Should not allow to enable token already enabled", async function () {
+      await truffleAssert.reverts(
+        handler.enableTokens([USDC.address]),
+        "enableToken: Has been enabled!"
+      );
+    });
+  });
+
+  describe("approve", function () {
+    before(async function () {
+      await resetContracts();
+    });
+
+    it("Should only allow auth to approve", async function () {
+      await handler.approve(USDC.address);
+      let allowance = await USDC.allowance(handler.address, mock_dtoken);
+      assert.equal(allowance.eq(UINT256_MAX), true);
+
+      await truffleAssert.reverts(
+        handler.approve(USDC.address, {
+          from: account1,
+        }),
+        "ds-auth-unauthorized"
+      );
+    });
+  });
+
+  describe("deposit", function () {
+    before(async function () {
+      await resetContracts();
+      await handler.approve(USDC.address);
+    });
+
+    it("Should only allow auth to deposit", async function () {
+      let amount = await handler.deposit(USDC.address, 1000e6);
+
+      //TODO: Check returen value from transaction
+      //console.log(JSON.stringify(amount));
+      //assert.equal(amount.eq(new BN(1000e6)), true);
+
+      await truffleAssert.reverts(
+        handler.deposit(USDC.address, 1000e6, {
+          from: account1,
+        }),
+        "ds-auth-unauthorized"
+      );
+    });
+
+    it("Should not deposit with disabled token", async function () {
+      await truffleAssert.reverts(
+        handler.deposit(mock_dtoken, 1000e6),
+        "deposit: Token is disabled!"
+      );
+    });
+
+    it("Should not deposit when paused", async function () {
+      await handler.pause();
+      await truffleAssert.reverts(
+        handler.deposit(USDC.address, 1000e6),
+        "whenNotPaused: paused"
+      );
+    });
+
+    it("!! TODO: Should not be able to reenter", async function () {});
+  });
+
+  describe("withdraw", function () {
+    beforeEach(async function () {
+      await resetContracts();
+      await handler.deposit(USDC.address, 1000e6);
+    });
+
+    it("Should only allow auth to withdraw", async function () {
+      let amount = await handler.withdraw(USDC.address, 100e6);
+
+      //TODO: Check returen value from transaction
+      //console.log(JSON.stringify(amount));
+      //assert.equal(amount.eq(new BN(1000e6)), true);
+
+      await truffleAssert.reverts(
+        handler.withdraw(USDC.address, 1000e6, {
+          from: account1,
+        }),
+        "ds-auth-unauthorized"
+      );
+    });
+
+    it("Should not withdraw when paused", async function () {
+      await handler.pause();
+      await truffleAssert.reverts(
+        handler.withdraw(USDC.address, 1000e6),
+        "whenNotPaused: paused"
+      );
+    });
+
+    it("!! TODO: Should not be able to reenter", async function () {});
+  });
+
+  describe("getBalance", function () {
+    beforeEach(async function () {
+      await resetContracts();
+      await handler.deposit(USDC.address, 1000e6);
+    });
+
+    it("Should get some balance", async function () {
+      let balance = await handler.getBalance(USDC.address);
+      assert.equal(balance.eq(new BN(1000e6)), true);
+    });
+  });
+
+  describe("getLiquidity", function () {
+    beforeEach(async function () {
+      await resetContracts();
+      await handler.deposit(USDC.address, 1000e6);
+    });
+
+    it("Should get some liquidity", async function () {
+      let balance = await handler.getLiquidity(USDC.address);
+      assert.equal(balance.eq(new BN(1000e6)), true);
+    });
+  });
+
+  describe("getRealBalance", function () {
+    beforeEach(async function () {
+      await resetContracts();
+      await handler.deposit(USDC.address, 1000e6);
+    });
+
+    it("Should get some real balance", async function () {
+      let balance = await handler.getRealBalance(USDC.address);
+
+      //TODO: Check returen value from transaction
+      //console.log(JSON.stringify(balance));
+      //assert.equal(balance.eq(new BN(1000e6)), true);
+    });
+  });
+});
---
+++ test/testDTokenIntegration.js
@@ -0,0 +1,248 @@
+const truffleAssert = require("truffle-assertions");
+const FiatToken = artifacts.require("FiatTokenV1");
+const CToken = artifacts.require("CTokenMockup");
+const CompoundHandler = artifacts.require("CompoundHandler");
+const InternalHandler = artifacts.require("InternalHandler");
+const Dispatcher = artifacts.require("Dispatcher");
+const dTokenAddresses = artifacts.require("dTokenAddresses");
+const DToken = artifacts.require("DToken");
+const DSGuard = artifacts.require("DSGuard");
+const ZERO_ADDR = "0x0000000000000000000000000000000000000000";
+const BN = require("bn.js");
+
+const UINT256_MAX = new BN(2).pow(new BN(256)).sub(new BN(1));
+const BASE = new BN(10).pow(new BN(18));
+
+describe("DToken Contract Integration", function () {
+  let owner, account1, account2, account3, account4;
+  let USDC, USDT;
+  let ds_guard;
+  let dispatcher;
+  let dtoken_addresses;
+  let internal_handler, compound_handler, aave_handler;
+  let dUSDC, dUSDT;
+
+  before(async function () {
+    [
+      owner,
+      account1,
+      account2,
+      account3,
+      account4,
+    ] = await web3.eth.getAccounts();
+  });
+
+  async function resetContracts() {
+    USDC = await FiatToken.new(
+      "USDC",
+      "USDC",
+      "USD",
+      6,
+      owner,
+      owner,
+      owner,
+      owner
+    );
+
+    USDT = await FiatToken.new("USDT", "USDT", 6, owner, owner, owner, owner);
+
+    dtoken_addresses = await dTokenAddresses.new();
+    ds_guard = await DSGuard.new();
+
+    internal_handler = await InternalHandler.new(dtoken_addresses.address);
+
+    let cUSDT = await CToken.new("cUSDT", "cUSDT", USDT.address);
+    let cUSDC = await CToken.new("cUSDC", "cUSDC", USDC.address);
+
+    compound_handler = await CompoundHandler.new(dtoken_addresses.address);
+    await compound_handler.setcTokensRelation(
+      [USDT.address, USDC.address],
+      [cUSDT.address, cUSDC.address]
+    );
+
+    aave_handler = await InternalHandler.new(dtoken_addresses.address);
+
+    // Use internal handler by default
+    dispatcher = await Dispatcher.new([internal_handler.address], [1000000]);
+    dUSDC = await DToken.new(
+      "dUSDC",
+      "dUSDC",
+      USDC.address,
+      dispatcher.address
+    );
+    dUSDT = await DToken.new(
+      "dUSDT",
+      "dUSDT",
+      USDT.address,
+      dispatcher.address
+    );
+
+    await dtoken_addresses.setdTokensRelation(
+      [USDC.address, USDT.address],
+      [dUSDC.address, dUSDT.address]
+    );
+
+    await dUSDC.setAuthority(ds_guard.address);
+    await dUSDT.setAuthority(ds_guard.address);
+    await dispatcher.setAuthority(ds_guard.address);
+
+    // Initialize all handlers
+    let handlers = [internal_handler, compound_handler, aave_handler];
+    for (const handler of handlers) {
+      await handler.setAuthority(ds_guard.address);
+      await handler.approve(USDC.address);
+      await handler.approve(USDT.address);
+      await ds_guard.permitx(dUSDC.address, handler.address);
+
+      await handler.enableTokens([USDC.address, USDT.address]);
+    }
+
+    // Allocate some token to all accounts
+    let accounts = [account1, account2, account3, account4];
+    for (const account of accounts) {
+      await USDC.allocateTo(account, 100000e6);
+      await USDT.allocateTo(account, 100000e6);
+      USDC.approve(dUSDC.address, UINT256_MAX, { from: account });
+      USDT.approve(dUSDT.address, UINT256_MAX, { from: account });
+    }
+  }
+
+  async function getAllTokenBalances(account) {
+    let balances = {};
+
+    balances.usdc = await USDC.balanceOf(account);
+    balances.usdt = await USDT.balanceOf(account);
+    balances.dusdc = await dUSDC.balanceOf(account);
+    balances.dusdt = await dUSDT.balanceOf(account);
+
+    //console.log(balances);
+
+    return balances;
+  }
+
+  async function getAllLiquidities() {
+    let liquidities = {};
+
+    liquidities.int_usdc = await internal_handler.getLiquidity(USDC.address);
+    liquidities.com_usdc = await compound_handler.getLiquidity(USDC.address);
+    liquidities.aav_usdc = await aave_handler.getLiquidity(USDC.address);
+
+    liquidities.int_usdt = await internal_handler.getLiquidity(USDT.address);
+    liquidities.com_usdt = await compound_handler.getLiquidity(USDT.address);
+    liquidities.aav_usdt = await aave_handler.getLiquidity(USDT.address);
+
+    //console.log(liquidities);
+
+    return liquidities;
+  }
+
+  async function calcDiff(asyncFn, account) {
+    let diff = {};
+
+    let balances = await getAllTokenBalances(account);
+    let liq = await getAllLiquidities();
+
+    await asyncFn;
+
+    let new_balances = await getAllTokenBalances(account);
+    let new_liq = await getAllLiquidities();
+
+    diff.usdc = new_balances.usdc.sub(balances.usdc).toString();
+    diff.usdt = new_balances.usdt.sub(balances.usdt).toString();
+    diff.dusdc = new_balances.dusdc.sub(balances.dusdc).toString();
+    diff.dusdt = new_balances.dusdt.sub(balances.dusdt).toString();
+    diff.int_usdc = new_liq.int_usdc.sub(liq.int_usdc).toString();
+    diff.com_usdc = new_liq.com_usdc.sub(liq.com_usdc).toString();
+    diff.aav_usdc = new_liq.aav_usdc.sub(liq.aav_usdc).toString();
+    diff.int_usdt = new_liq.int_usdt.sub(liq.int_usdt).toString();
+    diff.com_usdt = new_liq.com_usdt.sub(liq.com_usdt).toString();
+    diff.aav_usdt = new_liq.aav_usdt.sub(liq.aav_usdt).toString();
+
+    console.log(diff);
+
+    return diff;
+  }
+
+  describe("DToken Integration: Only internal handler", function () {
+    beforeEach(async function () {
+      await resetContracts();
+    });
+
+    it("Case 1", async function () {
+      await truffleAssert.reverts(
+        dispatcher.resetHandler([internal_handler.address], [10000]),
+        "the sum of propotions must be 1000000"
+      );
+
+      await truffleAssert.reverts(
+        dispatcher.updatePropotion([internal_handler.address], [10000]),
+        "the sum of propotions must be 1000000"
+      );
+
+      // We need to mint some in order to burn
+      await dUSDC.mint(account1, 1000e6, { from: account1 });
+
+      await internal_handler.disableTokens([USDC.address]);
+      await truffleAssert.reverts(
+        dUSDC.mint(account1, 1000e6, { from: account1 }),
+        "deposit: Token is disabled!"
+      );
+
+      await internal_handler.enableTokens([USDC.address]);
+      await dUSDC.mint(account1, 1000e6, { from: account1 });
+    });
+  });
+
+  describe("DToken Integration: internal and compound handler ", async function () {
+    beforeEach(async function () {
+      await resetContracts();
+    });
+
+    it("Case 1", async function () {
+      let amount = new BN(1000e6);
+      await dispatcher.addHandler([compound_handler.address]);
+
+      let balance = await dUSDC.balanceOf(account1);
+      let liq = await internal_handler.getLiquidity(USDC.address);
+      console.log(balance.toString());
+      console.log(liq.toString());
+
+      let diff = await calcDiff(
+        dUSDC.mint(account1, amount, { from: account1 }),
+        account1
+      );
+
+      balance = await dUSDC.balanceOf(account1);
+      console.log(balance.toString());
+
+      //assert.equal(diff.usdc, "-" + amount.toString());
+      //assert.equal(diff.dusdc, amount.toString());
+      //assert.equal(diff.int_usdc, amount.toString());
+
+      await dispatcher.updatePropotion(
+        [internal_handler.address, compound_handler.address],
+        [900000, 100000]
+      );
+
+      diff = await calcDiff(
+        dUSDC.mint(account1, 1000e6, { from: account1 }),
+        account1
+      );
+
+      balance = await dUSDC.balanceOf(account1);
+      console.log(balance.toString());
+
+      liq = await internal_handler.getLiquidity(USDC.address);
+      console.log(liq.toString());
+
+      await dUSDC.mint(account1, 1000e6, { from: account1 });
+    });
+  });
+
+  describe("DToken Integration: internal, compound and avee handler ", async function () {
+    beforeEach(async function () {
+      await resetContracts();
+    });
+    it("Case 1", async function () {});
+  });
+});
